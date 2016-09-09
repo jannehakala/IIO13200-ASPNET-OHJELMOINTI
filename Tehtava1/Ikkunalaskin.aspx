a38e07dd4868604ecd24bc11cee4a125c00092cd
jannehakala <janne.hakala94@hotmail.fi>
2016-09-09 16:40:49 +0300

    Finish Tehtava1

--- Tehtava1/Ikkunalaskin.aspx
+++ Tehtava1/Ikkunalaskin.aspx
@@ -9,19 +9,26 @@
 <body>
     <form id="form1" runat="server">
     <div>
-        <asp:Label ID="lblWindowWidth" runat="server" Text="Ikkunan leveys (L):"></asp:Label>
+        <asp:Label ID="lblWindowWidth" runat="server" Text="Ikkunan leveys (mm):"></asp:Label>
         <asp:TextBox ID="txtWindowWidth" runat="server"></asp:TextBox><br /><br />
-        <asp:Label ID="lblWindowHeight" runat="server" Text="Ikkunan korkeus (H):"></asp:Label>
+
+        <asp:Label ID="lblWindowHeight" runat="server" Text="Ikkunan korkeus (mm):"></asp:Label>
         <asp:TextBox ID="txtWindowHeight" runat="server"></asp:TextBox><br /><br />
-        <asp:Label ID="lblFrameWidth" runat="server" Text="Karmipuun leveys (W):"></asp:Label>
-        <asp:TextBox ID="txtFrameWidth" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Label ID="lblFrameWidth" runat="server" Text="Karmipuun leveys (mm):"></asp:Label>
+        <asp:TextBox ID="txtFrameWidth" runat="server">45</asp:TextBox><br /><br />
+
         <asp:Button ID="btnCalculatePrice" runat="server" Text="Laske tarjoushinta" OnClick="btnCalculatePrice_Click" /><br /><br />
         <asp:Label ID="lblWindowArea" runat="server" Text="Ikkunan pinta-ala:"></asp:Label>
+
         <asp:Label ID="lblResultWindowArea" runat="server" Text=""></asp:Label><br /><br />
         <asp:Label ID="lblFrameCircuit" runat="server" Text="Karmin piiri:"></asp:Label>
+
         <asp:Label ID="lblResultFrameCircuit" runat="server" Text=""></asp:Label><br /><br />
         <asp:Label ID="lblPrice" runat="server" Text="Tarjoushinta (ilman ALV):"></asp:Label>
         <asp:Label ID="lblResultPrice" runat="server" Text=""></asp:Label><br /><br />
+
+        <asp:Label ID="lblMessage" runat="server" Text="Ei ilmoituksia." Font-Bold="True"></asp:Label>
     </div>
     </form>
 </body>
--- Tehtava1/Ikkunalaskin.aspx.cs
+++ Tehtava1/Ikkunalaskin.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,12 +15,39 @@ public partial class Ikkunalaskin : System.Web.UI.Page {
         double WindowWidth;
         double WindowHeight;
         double FrameWidth;
+        double area;
+        double perimeter;
+        double price;
+        double margin;
+        double windowsquareprice;
+        double frameprice;
+        double workcost;
 
         try {
+            WindowWidth = double.Parse(txtWindowWidth.Text);
+            WindowHeight = double.Parse(txtWindowHeight.Text);
+            FrameWidth = double.Parse(txtFrameWidth.Text);
+
+            margin = double.Parse(ConfigurationManager.AppSettings["margin"]);
+            windowsquareprice = double.Parse(ConfigurationManager.AppSettings["windowsquareprice"]);
+            frameprice = double.Parse(ConfigurationManager.AppSettings["frameprice"]);
+            workcost = double.Parse(ConfigurationManager.AppSettings["workcost"]);
+
+            area = WindowWidth * WindowHeight / 1000000;
+            lblResultWindowArea.Text = area.ToString("0.##") + " m^2";
+
+            perimeter = (((WindowWidth * 2) + (WindowHeight * 2) + FrameWidth * 4) / 1000);
+            lblResultFrameCircuit.Text = perimeter.ToString("0.##") + " m";
+
+            price = ((1 + (margin / 100)) * (area * windowsquareprice) + (perimeter * frameprice) + (workcost));
+            lblResultPrice.Text = price.ToString("0.##") + " €";
+
+            lblMessage.Text = "Laskenta onnistui!";
+            lblMessage.ForeColor = System.Drawing.Color.Black;
 
         } catch (Exception) {
-
-            throw;
+            lblMessage.Text = "Käytit vääriä merkkejä tai jokin kenttä jäi tyhjäksi.";
+            lblMessage.ForeColor = System.Drawing.Color.Red;
         }
     }
 }
